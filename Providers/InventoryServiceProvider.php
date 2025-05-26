78eccf68e4c922a9c15be1449464b435300c06b1
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-26 18:09:18 +0700

    update case sensitive

--- Providers/InventoryServiceProvider.php
+++ Providers/InventoryServiceProvider.php
@@ -8,7 +8,7 @@ class InventoryServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../Databases/Migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../Databases/migrations');
         $this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
         // $this->loadViewsFrom(__DIR__.'/../Views', 'base');
     }
